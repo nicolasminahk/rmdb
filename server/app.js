e3312520d55c51061776e1de020f6ea666c1a799
nicolasminahk <nico.minahk@gmail.com>
2022-08-30 19:31:49 -0300

    app

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
+const port = 3000;
 
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Bienvenid@s a MondongoTV");
+});
+
+app.listen(port, () => {
+  console.log(`El servidor esta escuchando en el puerto  ${port}`);
+});
